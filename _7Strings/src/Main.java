baebf3c4e841adafd83ba114a4280b5dd3dc3500
ManuelBasso <manuel.basso.89@gmail.com>
2023-10-26 10:53:12 +0200

    fix unicode conversion

--- _7Strings/src/Main.java
+++ _7Strings/src/Main.java
@@ -2,19 +2,22 @@ import java.util.concurrent.LinkedBlockingDeque;
 
 public class Main {
     public static void main(String[] args) {
-        String stringa = "Ciao";
+
+        String stringa = "αCiao";
         int index =0;
-            StringBuilder str = new StringBuilder("Stringa: " + stringa + "\nStampare unicode del carattere nella posizione specificata.\n");
+
+        StringBuilder str = new StringBuilder("Stringa: " + stringa + "\nStampare unicode del carattere nella posizione specificata.\n");
 
         if ( index >= stringa.length() || index < 0 ) {
             str.append("Errore, inserisci altro indice");
         } else {
             printUnicode(stringa, index);
             str.append("Il carattere nella posizione " + index + " corrisponde al codice unicode: ");
-            str.append(printUnicode(stringa, index));
+            str.append(String.format("\\u%x", printUnicode(stringa, index)));
         }
 
             System.out.println(str);
+
     }
 
     static int printUnicode(String a, int i){
